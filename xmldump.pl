c5f29faabe69f64bbbceb46d107e25d54c06b64d
Chad Carr <ccarr@franzdoodle.com>
2019-03-10 11:02:09 -0400

    Script working

--- xmldump.pl
+++ xmldump.pl
@@ -2,21 +2,6 @@
 
 use XML::Writer;
 
-my $data = {
-    [
-        {
-            device => "/dev/sda1",
-            mount => "/boot",
-            free => "4M"
-        },
-        {
-            device => "/dev/sda2",
-            mount => "/",
-            free => "12G"
-        }
-    ]
-}
-
 my $writer = XML::Writer->new(OUTPUT => "self");
-$writer->dataElement($data);
-print($writer);
+$writer->dataElement( device => "/dev/sda1" );
+print("$writer\n");
